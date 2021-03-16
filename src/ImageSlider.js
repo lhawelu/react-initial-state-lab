97a76d197caa54963e63103e16aa44aae85dae2b
Larry Hawelu <lhawelu@gmail.com>
2021-03-16 16:41:01 -0700

    Assignment completed

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,19 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+
+export default class ImageSlider extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render() {
+    const current_slide = this.state.currentSlideIndex
+      return (
+        <div>I am on slide {current_slide}</div>
+      ) 
+  }
+}
